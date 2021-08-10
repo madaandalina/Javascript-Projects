0fab3add2cbb6f8570200a196b3e5c24f912d0ad
madaandalina <madalina.lenz@gmail.com>
2021-08-10 16:13:15 +0200

    + removing items

--- ProiecteGit/groceryListUdemy/script.js
+++ ProiecteGit/groceryListUdemy/script.js
@@ -2,19 +2,31 @@ const listForm = document.querySelector("#listForm");
 const list = document.querySelector("#list");
 const button = document.querySelector("#button");
 
+
+// take inputs from user and add to list
 button.addEventListener("click", function (e) {
   e.preventDefault();
   const product = listForm.elements.product;
   const quantity = listForm.elements.quantity;
+ 
 
-  writeList(product.value, quantity.value);
+  addItem(product.value, quantity.value);
   product.value = "";
   quantity.value = "";
 });
 
-function writeList(product, quantity) {
+function addItem(product, quantity) {
   const listItem = document.createElement("li");
   listItem.append(`${product} - ${quantity}`);
   list.append(listItem);
   return list;
 }
+
+// remove inputs from list EVENT DELEGATION ON PARENT ELEMENT
+list.addEventListener("click", function(e){
+    if(e.target.nodeName ==="LI"){
+        e.target.remove()
+    }
+  
+})
+
